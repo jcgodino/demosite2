608924928f1f34970d24b376356468aef6eec3a2
jcgodino <carlos.godino@me.com>
2013-12-06 14:36:40 +0000

    notification templates

--- js/notificationtemplate.js
+++ js/notificationtemplate.js
@@ -1,7 +1,37 @@
-/**
- * Created with IntelliJ IDEA.
- * User: ju005go
- * Date: 06/12/13
- * Time: 10:18
- * To change this template use File | Settings | File Templates.
- */
+var increment = 1;
+var selectedEventID = "";
+
+function getEmailTemplate(id) {
+	$("#"+selectedEventID).removeClass("active");
+	selectedEventID = id;
+	$("#"+selectedEventID).addClass("active");
+}
+
+function saveNotificationTemplate() {
+	alert("saveNotificationTemplate()");
+}
+
+function addNotificationEvent(eventName) {
+	theId = "eventID_" + increment;
+	$("#eventList").append("<a href=\"#\" id=\"" + theId + "\" class=\"list-group-item\">" + eventName + "</a>");
+
+	$(".list-group-item").click(function (event) {
+		getEmailTemplate(event.target.id);
+	});
+	increment += 1;
+}
+
+function removeNotificationEvent() {
+	alert("removeNotificationEvent()");
+}
+
+function eventSelected(e) {
+	alert("eventSelected()");
+}
+
+function setupNotificationEventHandlers() {
+	addNotificationEvent("Start Workflow");
+	addNotificationEvent("Approve");
+	addNotificationEvent("Reject");
+	addNotificationEvent("Workflow Completed");
+}
