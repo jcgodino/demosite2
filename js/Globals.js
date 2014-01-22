95ccc01a1765f54d0d8aaa1e182a64f8159043fa
jcgodino <carlos.godino@me.com>
2014-01-22 13:41:10 +0000

    notifications

--- js/Globals.js
+++ js/Globals.js
@@ -8,12 +8,14 @@ var serviceURL = hostURL + baseServiceURL;
 
 var viewpointAssetBasePath = "/viewpoint-services/v1/assets";
 var viewpointSSBasePath = "/viewpoint-services/v1/savedsearches";
-var viewpointWFBasePath = "/viewpoint-services/v1/resolution/rule";
+var viewpointWFBasePath = "/viewpoint-services/v1/resolution";
+var viewpointWFRuleBasePath = "/viewpoint-services/v1/resolution/rule";
 
 var viewpointSearchURL = hostURL + viewpointAssetBasePath;
 var viewpointSaveSearchURL = hostURL + viewpointSSBasePath;
 
 var viewpointWorkflowURL = hostURL + viewpointWFBasePath;
+var viewpointWorkflowRuleURL = hostURL + viewpointWFRuleBasePath;
 
 var incIndent = "&indent=true";
 var solrWriter = "&wt=json"
