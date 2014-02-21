bb2dc7ba42a30132ab023a46bdccb8ba9d2077bc
Chrodoma <dulandhanapala@gmail.com>
2014-02-21 11:18:05 +0000

    Worflow Precentage demo
    Rule selection Half baked

--- js/workflowdemo.js
+++ js/workflowdemo.js
@@ -23,6 +23,119 @@
 		    //alert(msg.responseText)
 		});
 }
+function createresolutionrule1() {
+
+    var jsonObj = {
+        //workflow ID needs to passed on from the previous panel
+        "workflowId": $("td:eq( 0 )").html(),
+        "associatedId": $("td:eq( 1 )").html(),
+        "type": $("td:eq( 2 )").html(),
+        "percentage": $("td:eq( 3 )").html()
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/viewpoint-services/v1/community/resolution/rules",
+        contentType: "application/json",
+        data: JSON.stringify(jsonObj),
+        dataType: 'json'
+    })
+		.done(function (data) {
+		    $('#R1').removeClass("alert-danger");
+		    $("#R1").html("Rule Created").addClass("alert alert-success").show()
+		})
+		.error(function (msg) {
+		    $('#R1').removeClass("alert-success");
+		    $("#R1").html(msg.responseText).addClass("alert alert-danger").show();
+		});
+
+
+}
+function createresolutionrule2() {
+
+    var jsonObj = {
+        //workflow ID needs to passed on from the previous panel
+        "workflowId": 2,
+        "associatedId": 55,
+        "type": "template",
+        "percentage": "40"
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/viewpoint-services/v1/community/resolution/rules",
+        contentType: "application/json",
+        data: JSON.stringify(jsonObj),
+        dataType: 'json'
+    })
+		.done(function (data) {
+		    $('#R2').removeClass("alert-danger");
+		    $("#R2").html("Rule Created").addClass("alert alert-success").show()
+		})
+		.error(function (msg) {
+		    $('#R2').removeClass("alert-success");
+		    $("#R2").html(msg.responseText).addClass("alert alert-danger").show();
+		});
+
+
+}
+
+function createresolutionrule3() {
+
+    var jsonObj = {
+        //workflow ID needs to passed on from the previous panel
+        "workflowId": 3,
+        "associatedId": 10,
+        "type": "submitter",
+        "percentage": "50"
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/viewpoint-services/v1/community/resolution/rules",
+        contentType: "application/json",
+        data: JSON.stringify(jsonObj),
+        dataType: 'json'
+    })
+		.done(function (data) {
+		    $('#R3').removeClass("alert-danger");
+		    $("#R3").html("Rule Created").addClass("alert alert-success").show()
+		})
+		.error(function (msg) {
+		    $('#R3').removeClass("alert-success");
+		    $("#R3").html(msg.responseText).addClass("alert alert-danger").show();
+		});
+
+
+}
+
+var j = 0;
+function getworkflowforsubmitter() {
+    for (var i = 0; i < 12;i++) {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/viewpoint-services/v1/community/resolution?template=" + "55" + "&submitter=" + "10",
+            dataType: 'json',
+            async: false
+        })
+		.done(function (data) {
+		    $('#Wid').removeClass("alert-danger");
+		    if (data.data.id == 3) {
+		        j = j + 1;
+		        console.log(data.data.id);
+		    }else
+		    console.log("Nothing executed");
+		    $("#Wid").html("Workflow to be executed " + data.data.id + " Was executed "+ j + " Times when a template is submitted 100 Times" ).addClass("alert alert-success").show();
+		})
+		.error(function (msg) {
+		    $('#Wid').removeClass("alert-success");
+		    $("#Wid").html(msg.responseText).addClass("alert alert-danger").show();
+		});
+
+   }
+  
+}
+
 function associatetemplate() {
 
     var jsonObj = {
