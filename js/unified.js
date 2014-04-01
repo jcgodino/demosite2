0da89ec10ebbfda5d69dea36b397469ba15d8902
jcgodino <carlos.godino@me.com>
2014-04-01 08:37:32 +0100

    Changes for the Unified list

--- js/unified.js
+++ js/unified.js
@@ -2,119 +2,22 @@
 var unifiedTaskListSortfield = "&sortField=TASKNAME"
 var currentReviewTaskInteractiveID = "";
 var reviewCommentsText = "";
+var page = "1";
+var pageParam = "&page=";
+var pageSize = "10";
+var pageSizeParam = "&size=";
 
-function createcomment() {
-	var d = new Date();
-	var jsonObj = {
-		"taskid": $("#taskid1").val(),
-		"timestamp": d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2),
-		"user": {
-			"id": $("#username").val(),
-			"first": "Volumnia",
-			"last": "Dedlock",
-			"title": "",
-			"role": "admin",
-			"email": "volumnia.dedlock@example.com",
-			"type": "user"
-		},
-		"comment": $("#comment").val(),
-		"status": $('input[name="approve"]:checked').val()
-	}
-	//console.log(d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
-	$.ajax({
-		type: "POST",
-		// url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments",
-		url: "http://localhost:8080/viewpoint-services/v1/community/comments",
-		contentType: "application/json",
-		data: JSON.stringify(jsonObj),
-		dataType: 'json'
-	})
-		.done(function (data) {
-			$('#c1').removeClass("alert-danger");
-			$("#c1").html("Comment Added").addClass("alert alert-success").show();
-			id1 = data.data.id;
-			console.log(id1);
-			$("#commentid").val(id1);
-			listcomment();
-		})
-		.error(function (msg) {
-			$('#c1').removeClass("alert-success");
-			$("#c1").html(msg.responseText).addClass("alert alert-danger").show();
-			console.log("something went wrong");
-			console.log(msg);
-		});
-
-
+function buildPaginationParams() {
+	return pageParam + pageSize + pageSizeParam + pageSize;
 }
 
-function getcomment() {
-
-	$.ajax({
-		type: "GET",
-		//url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments/" + $("#commentid").val(),
-		url: "http://localhost:8080/viewpoint-services/v1/community/comments/" + $("#commentid").val(),
-		contentType: "application/json",
-		dataType: 'json'
-	})
-		.done(function (data) {
-			$('#name').html("Comment : " + data.data.comment + "<br>").addClass("alert alert-success").show();
-			$('#name').append("Name : " + data.data.user.first + " " + data.data.user.last + "<br>").addClass("alert alert-success").show();
-			$('#name').append("Time Stamp : " + data.data.timestamp + "<br>").addClass("alert alert-success").show();
-			$('#name').append("Status : " + data.data.status + "<br>").addClass("alert alert-success").show();
-
-		})
-		.error(function (msg) {
-			$('#name').removeClass("alert-success");
-			$("#name").html(msg.responseText).addClass("alert alert-danger").show();
-			console.log("something went wrong");
-			console.log(msg);
-		});
-
-
-}
-
-
-function updatecomment() {
-	var d = new Date();
-	var jsonObj = {
-		"id": id1,
-		"taskid": "5e1d005e-9d58-11e3-8d05-425861b86ab6",
-		"timestamp": d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2),
-		"user": {
-			"id": "vo001de",
-			"first": "Volumnia",
-			"last": "Dedlock",
-			"title": "",
-			"role": "admin",
-			"email": "volumnia.dedlock@example.com",
-			"type": "user"
-		},
-		"comment": $("#comment").val(),
-		"status": $('input[name="approve"]:checked').val()
-	}
-	//console.log(d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes() ).slice(-2) + ':' +  ('0' + d.getSeconds()).slice(-2);
-	$.ajax({
-		type: "PUT",
-		//url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments/" + id1,
-		url: "http://localhost:8080/viewpoint-services/v1/community/comments/" + id1,
-		contentType: "application/json",
-		data: JSON.stringify(jsonObj),
-		dataType: 'json'
-	})
-		.done(function (data) {
-			$('#c1').removeClass("alert-danger");
-			$("#c1").html("Comment Updated").addClass("alert alert-success").show();
-			id1 = data.data.id;
-			console.log(id1);
-		})
-		.error(function (msg) {
-			$('#c1').removeClass("alert-success");
-			$("#c1").html(msg.responseText).addClass("alert alert-danger").show();
-			console.log("something went wrong");
-			console.log(msg);
-		});
-
-
+function getTaskName(task) {
+	taskName = "";
+	if (task.type === "interactive")
+		taskName = task.externalId;
+	else
+		taskName = task.name;
+	return taskName;
 }
 
 function searchForTask() {
@@ -122,7 +25,7 @@ function searchForTask() {
 	$.ajax({
 		type: "GET",
 		//url: "http://uksearchdev:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1",
-		url: "http://localhost:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1&taskName=" + $('#searchForTaskQuery').val() + unifiedTaskListSortfield,
+		url: "http://localhost:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1&taskName=" + $('#searchForTaskQuery').val() + unifiedTaskListSortfield + buildPaginationParams(),
 		contentType: "application/json",
 		dataType: 'json'
 	})
@@ -132,11 +35,11 @@ function searchForTask() {
 			console.log(foo.data);
 			var type;
 			$.each(foo.data.tasks, function (i, task) {
-				$('#tasklist').append('<li><a href="#" id="' + task.uriToGetTaskDetails + '" class="list-group-item"><span>Task Description : </span>' + task.description + '<br> Type :<i>' + task.type + '</i></a></li>');
+				$('#tasklist').append('<li><a href="#" id="' + task.uriToGetTaskDetails + '" class="list-group-item"><span>Task Description : </span>' + getTaskName(task) + '<br> Type :<i>' + task.type + '</i></a></li>');
 			});
 
 			$(".list-group-item").click(function (event) {
-				gettaskdetails(event.target.id);
+				getTaskDetails(event.target.id);
 			});
 
 		})
@@ -147,12 +50,13 @@ function searchForTask() {
 			console.log(msg);
 		});
 }
-function listunifiedtask() {
+
+function listUnifiedTask() {
 
 	$.ajax({
 		type: "GET",
 		//url: "http://uksearchdev:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1",
-		url: "http://localhost:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1" + unifiedTaskListSortfield,
+		url: "http://localhost:8080/viewpoint-services/v1/EngageOne/unified/tasks?userId=user1" + unifiedTaskListSortfield + buildPaginationParams(),
 		contentType: "application/json",
 		dataType: 'json'
 	})
@@ -162,16 +66,11 @@ function listunifiedtask() {
 			console.log(foo.data);
 			var type;
 			$.each(foo.data.tasks, function (i, task) {
-				taskName = "";
-				if (task.type === "interactive")
-					taskName = task.externalId;
-				else
-					taskName = task.name;
-				$('#tasklist').append('<li><a href="#" id="' + task.uriToGetTaskDetails + '" class="list-group-item"><span>Task Description : </span>' + taskName + '<br> Type :<i>' + task.type + '</i></a></li>');
+				$('#tasklist').append('<li><a href="#" id="' + task.uriToGetTaskDetails + '" class="list-group-item"><span>Task Description : </span>' + getTaskName(task) + '<br> Type :<i>' + task.type + '</i></a></li>');
 			});
 
 			$(".list-group-item").click(function (event) {
-				gettaskdetails(event.target.id);
+				getTaskDetails(event.target.id);
 			});
 
 		})
@@ -184,9 +83,8 @@ function listunifiedtask() {
 
 
 }
-var externalid;
 
-function gettaskdetails(uriToGetTaskDetails) {
+function getTaskDetails(uriToGetTaskDetails) {
 	$.ajax({
 		type: "GET",
 		url: "http://localhost:8080" + uriToGetTaskDetails,
@@ -282,7 +180,7 @@ function showReviewComments() {
 	$('#wkTaskComments').html(reviewCommentsText);
 }
 
-function saveComment(btnId) {
+function getStatus(btnId) {
 	status = "";
 	if (btnId === "btnApprove") {
 		status = "approved";
@@ -290,34 +188,16 @@ function saveComment(btnId) {
 	else if (btnId === "btnReject") {
 		status = "Rejected";
 	}
+}
 
-	var d = new Date();
-	//timeStampNow =  d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
-	timeStampNow = d.toISOString().substring(0, d.toISOString().length - 5);
-	//alert(timeStampNow);
-
-	var jsonObj = {
-		"taskid": currentReviewTaskInteractiveID,
-		"timestamp": timeStampNow,
-		"user": {
-			"id": "vo001de",
-			"first": "Volumnia",
-			"last": "Dedlock",
-			"title": "",
-			"role": "admin",
-			"email": "volumnia.dedlock@example.com",
-			"type": "user"
-		},
-		"comment": $("#wfCommentInput").val(),
-		"status": status
-	}
-
+function saveComment(btnId) {
+	getStatus(btnId);
 	$.ajax({
 		type: "POST",
 		//url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments/" + id1,
 		url: "http://localhost:8080/viewpoint-services/v1/EngageOne/comments",
 		contentType: "application/json",
-		data: JSON.stringify(jsonObj),
+		data: JSON.stringify(buildCommentJson(status)),
 		dataType: 'json'
 	})
 		.done(function (data) {
@@ -334,6 +214,7 @@ function saveComment(btnId) {
 		});
 }
 
+var externalid;
 function createworkflow2() {
 
 	var d = new Date();
@@ -398,6 +279,123 @@ function initiateworkflow(id) {
 
 		})
 		.error(function (msg) {
+			console.log("something went wrong");
+			console.log(msg);
+		});
+
+
+}
+
+function buildCommentJson(status) {
+	var d = new Date();
+	timeStampNow = d.toISOString().substring(0, d.toISOString().length - 5);
+	return {
+		"taskid": currentReviewTaskInteractiveID,
+		"timestamp": timeStampNow,
+		"user": {
+			"id": "vo001de",
+			"first": "Volumnia",
+			"last": "Dedlock",
+			"title": "",
+			"role": "admin",
+			"email": "volumnia.dedlock@example.com",
+			"type": "user"
+		},
+		"comment": $("#wfCommentInput").val(),
+		"status": status
+	}
+}
+
+function createcomment() {
+	//console.log(d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
+	$.ajax({
+		type: "POST",
+		// url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments",
+		url: "http://localhost:8080/viewpoint-services/v1/community/comments",
+		contentType: "application/json",
+		data: JSON.stringify(buildCommentJson()),
+		dataType: 'json'
+	})
+		.done(function (data) {
+			$('#c1').removeClass("alert-danger");
+			$("#c1").html("Comment Added").addClass("alert alert-success").show();
+			id1 = data.data.id;
+			console.log(id1);
+			$("#commentid").val(id1);
+			listcomment();
+		})
+		.error(function (msg) {
+			$('#c1').removeClass("alert-success");
+			$("#c1").html(msg.responseText).addClass("alert alert-danger").show();
+			console.log("something went wrong");
+			console.log(msg);
+		});
+
+
+}
+
+function getcomment() {
+
+	$.ajax({
+		type: "GET",
+		//url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments/" + $("#commentid").val(),
+		url: "http://localhost:8080/viewpoint-services/v1/community/comments/" + $("#commentid").val(),
+		contentType: "application/json",
+		dataType: 'json'
+	})
+		.done(function (data) {
+			$('#name').html("Comment : " + data.data.comment + "<br>").addClass("alert alert-success").show();
+			$('#name').append("Name : " + data.data.user.first + " " + data.data.user.last + "<br>").addClass("alert alert-success").show();
+			$('#name').append("Time Stamp : " + data.data.timestamp + "<br>").addClass("alert alert-success").show();
+			$('#name').append("Status : " + data.data.status + "<br>").addClass("alert alert-success").show();
+
+		})
+		.error(function (msg) {
+			$('#name').removeClass("alert-success");
+			$("#name").html(msg.responseText).addClass("alert alert-danger").show();
+			console.log("something went wrong");
+			console.log(msg);
+		});
+
+
+}
+
+function updatecomment() {
+	var d = new Date();
+	var jsonObj = {
+		"id": id1,
+		"taskid": "5e1d005e-9d58-11e3-8d05-425861b86ab6",
+		"timestamp": d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2),
+		"user": {
+			"id": "vo001de",
+			"first": "Volumnia",
+			"last": "Dedlock",
+			"title": "",
+			"role": "admin",
+			"email": "volumnia.dedlock@example.com",
+			"type": "user"
+		},
+		"comment": $("#comment").val(),
+		"status": $('input[name="approve"]:checked').val()
+	}
+	//console.log(d.getFullYear() + '-02-' + d.getDate() + 'T' + d.getHours() + ':' + ('0' + d.getMinutes() ).slice(-2) + ':' +  ('0' + d.getSeconds()).slice(-2);
+	$.ajax({
+		type: "PUT",
+		//url: "http://uksearchdev:8080/viewpoint-services/v1/community/comments/" + id1,
+		url: "http://localhost:8080/viewpoint-services/v1/community/comments/" + id1,
+		contentType: "application/json",
+		data: JSON.stringify(jsonObj),
+		dataType: 'json'
+	})
+		.done(function (data) {
+			$('#c1').removeClass("alert-danger");
+			$("#c1").html("Comment Updated").addClass("alert alert-success").show();
+			id1 = data.data.id;
+			console.log(id1);
+		})
+		.error(function (msg) {
+			$('#c1').removeClass("alert-success");
+			$("#c1").html(msg.responseText).addClass("alert alert-danger").show();
 			console.log("something went wrong");
 			console.log(msg);
 		});
